f167858957a8f30b043b4ee496f99b37a60b36aa
Langl3y <hieuhovan954@gmail.com>
2024-12-01 04:30:42 +0700

    Make the MovePlate sprite red to indicate attacking target(s)

--- Assets/Scripts/MovePlate.cs
+++ Assets/Scripts/MovePlate.cs
@@ -2,50 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlate : MonoBehaviour{
+public class MovePlate : MonoBehaviour
+{
     public GameObject controller;
     GameObject reference = null;
     int matrixX;
     int matrixY;
     public bool attack = false;
 
-    public void Start(){
-        if(attack)
+    public void Start()
+    {
+        if (attack)
         {
             gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
         }
     }
 
-    public void OnMouseUp(){
+    public void OnMouseUp()
+    {
         controller = GameObject.FindGameObjectWithTag("GameController");
-        if(attack)
+
+        if (attack)
         {
             GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
-            if (cp.name == "white_king") controller.GetComponent<Game>().Winner("black");
-            if (cp.name == "black_king") controller.GetComponent<Game>().Winner("white");
+            if (cp != null)
+            {
+                if (cp.name == "white_king") controller.GetComponent<Game>().Winner("black");
+                if (cp.name == "black_king") controller.GetComponent<Game>().Winner("white");
 
-            Destroy(cp);
+                Destroy(cp);
+            }
         }
-        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(),reference.GetComponent<Chessman>().GetYBoard());
-        reference.GetComponent<Chessman>().SetXBoard(matrixX);
-        reference.GetComponent<Chessman>().SetYBoard(matrixY);
-        reference.GetComponent<Chessman>().SetCoords();
 
-        controller.GetComponent<Game>().SetPosition(reference);
+        // Update the old position to empty
+        if (reference != null)
+        {
+            controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(), reference.GetComponent<Chessman>().GetYBoard());
+            reference.GetComponent<Chessman>().SetXBoard(matrixX);
+            reference.GetComponent<Chessman>().SetYBoard(matrixY);
+            reference.GetComponent<Chessman>().SetCoords();
+
+            // Update the new position
+            controller.GetComponent<Game>().SetPosition(reference);
+        }
+
+        // Destroy all move plates
+        reference.GetComponent<Chessman>().DestroyMovePlates();
+
+        // Switch turns
         controller.GetComponent<Game>().NextTurn();
-        controller.GetComponent<Chessman>().DestroyMovePlates();
+
+        // Destroy this move plate
+        Destroy(gameObject);
     }
 
-    public void SetCoords(int x, int y){
+    public void SetCoords(int x, int y)
+    {
         matrixX = x;
         matrixY = y;
     }
 
-    public void SetReference(GameObject obj){
+    public void SetReference(GameObject obj)
+    {
         reference = obj;
     }
 
-    public GameObject GetReference(){
+    public GameObject GetReference()
+    {
         return reference;
     }
 }
